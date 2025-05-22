3f67e8ed90c8db5b92c756086018021dcedd0c38
Bharath Dubbaka <bharath.reactdev@gmail.com>
2025-05-22 23:55:12 +0530

    added feed API with userCards

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/store/feedSlice";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+   const [error, setError] = useState("");
+   const dispatch = useDispatch();
+   const feed = useSelector((store) => store?.feed);
 
-export default Feed
+   const getFeed = async () => {
+      if (feed) return;
+      console.log(feed, "feed from store");
+      try {
+         const res = await axios.get(BASE_URL + "/feed", {
+            withCredentials: true,
+         });
+         console.log(res, "FEED res");
+         dispatch(addFeed(res?.data?.data));
+      } catch (error) {
+         setError(error?.response?.data || "Something went wrong");
+         console.log(error);
+      }
+   };
+   useEffect(() => {
+      getFeed();
+   }, []);
+
+   return (
+      <div>
+         <div className=" pb-20 flex flex-col items-center">
+            {feed?.map((f, index) => {
+               return (
+                  <div key={index} className="text-white">
+                     <UserCard user={f} />
+                  </div>
+               );
+            })}
+         </div>
+      </div>
+   );
+};
+
+export default Feed;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const UserCard = (user) => {
+   console.log(user, "user from UserCard");
+   const { firstName, lastName, age, gender, about, photoUrl } = user?.user;
+   return (
+      <div>
+         <div className="card bg-base-200 h-[36rem] w-96 shadow-sm my-10">
+            <figure>
+               <img src={photoUrl} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+               <h2 className="card-title">
+                  {firstName + " " + lastName}
+                  {age && gender && (
+                     <div className="badge badge-secondary">
+                        {gender + ", " + age}
+                     </div>
+                  )}
+               </h2>
+               <p>{about}</p>
+               <div className="card-actions justify-end">
+                  <button className=" btn btn-primary">Ignore</button>
+                  <button className=" btn btn-secondary">Interested</button>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default UserCard;
